bfaa8ac058af7e89d0d212393fd0975dfa8222e1
Vilius Stankaitis <vilius@stankaitis.com>
2021-01-08 09:53:37 +0000

    #27 Add Guide

--- src/Home/Guide/index.jsx
+++ src/Home/Guide/index.jsx
@@ -1,13 +1,91 @@
 import React from 'react';
 import { Page } from '@apps';
+import { IonItemDivider, IonContent } from '@ionic/react';
+import { Trans as T } from 'react-i18next';
 import Header from 'Components/Header';
+import Card from './Component/Card';
+import img1 from './images/image1.jpg';
+import img2 from './images/image2.jpg';
+import img3 from './images/image3.jpg';
+import img4 from './images/image4.jpg';
+import img5 from './images/image5.jpg';
+import img6 from './images/image6.jpg';
+import img7 from './images/image7.jpg';
+import img8 from './images/image8.jpg';
+import './styles.scss';
+
+const bgCards = [
+  {
+    text:
+      'If you are still not sure after looking at this guide please submit the record anyway',
+    image: img1,
+  },
+  {
+    text: 'Form paint-like surface scums often along shorelines',
+    image: img2,
+  },
+  {
+    text:
+      'Tiny irregular-shaped clumps (less than 3 mm) suspended in water. May be small and round or tiny grass like flakes',
+    image: img3,
+  },
+  {
+    text: 'Turquoise colour sometimes visible when decaying',
+    image: img4,
+  },
+];
+
+const nonBgCards = [
+  {
+    text: 'Plants that float but have solid structure such as leaves',
+    image: img5,
+  },
+  {
+    text: 'Others are long and tubular the size of a little finger',
+    image: img6,
+  },
+  {
+    text: 'Some have very stringy, long hair-like strands',
+    image: img7,
+  },
+  {
+    text: 'Or fine green hairs attached to rocks and pebbles',
+    image: img8,
+  },
+];
+
+const getCards = data => {
+  const showCards = card => <Card key={card.text} card={card} />;
+
+  return data.map(showCards);
+};
 
 function Guide() {
   return (
     <Page id="guide">
-      <Header title="identify" styleId="none" />
+      <Header title="Identify" styleId="none" />
+      <IonContent>
+        <div className="info-message">
+          <p>
+            <T>
+              If you are still not sure after looking at this guide please
+              submit the record anyway
+            </T>
+          </p>
+        </div>
 
-      <p>todo:</p>
+        <IonItemDivider>
+          <T>Blue-Green Algae Blooms</T>
+        </IonItemDivider>
+
+        {getCards(bgCards)}
+
+        <IonItemDivider>
+          <T>Non Blue-Green Algae</T>
+        </IonItemDivider>
+
+        {getCards(nonBgCards)}
+      </IonContent>
     </Page>
   );
 }
