9870711fd97c5f6ceeff3c6b48cb96f79ba2e29f
Vilius Stankaitis <vilius@stankaitis.com>
2021-01-24 11:12:36 +0000

    Fix survey verification

--- src/Survey/config.js
+++ src/Survey/config.js
@@ -165,7 +165,7 @@ const survey = {
         attrs: Yup.object()
           .shape({
             media: Yup.array()
-              .length(1, 'Please add a photo of the bloom')
+              .min(1, 'Please add a photo of the bloom')
               .required(),
 
             attrs: Yup.object().shape({
