ae59cbd3d2d9a897eff0c46a88f7c925b2a75e6b
Karolis Kazlauskis <karolis@flumens.io>
2021-03-22 16:34:46 +0000

    Improve onboarding slide design

--- src/Info/SplashScreenRequired/index.jsx
+++ src/Info/SplashScreenRequired/index.jsx
@@ -1,14 +1,24 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import PropTypes from 'prop-types';
 import { Page, Main } from '@apps';
 import { observer } from 'mobx-react';
-import { IonSlides, IonSlide, IonButton, IonIcon } from '@ionic/react';
+import {
+  IonSlides,
+  IonSlide,
+  IonButton,
+  IonIcon,
+  IonButtons,
+  IonToolbar,
+  IonFooter,
+} from '@ionic/react';
 import Log from 'helpers/log';
-import { alertCircle, arrowForward } from 'ionicons/icons';
+import { arrowForward, alertCircleOutline } from 'ionicons/icons';
 import { Trans as T } from 'react-i18next';
 import './styles.scss';
 
 const SplashScreen = ({ appModel }) => {
+  const [isEnd, setIsEnd] = useState(false);
+
   function exit() {
     Log('Info:Welcome:Controller: exit.');
     // eslint-disable-next-line no-param-reassign
@@ -24,70 +34,86 @@ const SplashScreen = ({ appModel }) => {
     e.target.update();
   };
 
+  const handleSlideChangeStart = async () => {
+    const isThisLastSlide = await slideRef.current.isEnd();
+    setIsEnd(isThisLastSlide);
+  };
+
+  const slideNextOrClose = async () => {
+    slideRef.current.swiper.slideNext();
+  };
+
   return (
     <Page id="welcome-page">
       <Main>
         <IonSlides
           pager
           ref={slideRef}
+          onIonSlideWillChange={handleSlideChangeStart}
           onIonSlidesDidLoad={onIonSlidesDidLoadWrap}
         >
           <IonSlide className="first">
-            <div className="slide-header">
-              <div className="message-blur-container">
-                <div className="message">
-                  <h2>
-                    <T>Welcome to Bloomin’ Algae!</T>
-                  </h2>
-                  <p>
-                    <T>
-                      Bloomin’ Algae is a Citizen Science app for reporting the
-                      presence of harmful blooms of blue-green algae . The
-                      records you send in help speed up warnings to minimise
-                      risks to public and animal health. Thanks for taking part!
-                    </T>
-                  </p>
-                </div>
+            <div className="message-container blur">
+              <div className="message-header">
+                <h2>
+                  <T>Welcome to Bloomin’ Algae!</T>
+                </h2>
               </div>
+
+              <p>
+                <T>
+                  Bloomin’ Algae is a Citizen Science app for reporting the
+                  presence of harmful blooms of blue-green algae. The records
+                  you send in help speed up warnings to minimise risks to public
+                  and animal health. Thanks for taking part!
+                </T>
+              </p>
             </div>
           </IonSlide>
 
           <IonSlide className="second">
-            <div className="slide-header">
-              <div className="message-warning">
-                <div className="alert-header">
-                  <IonIcon icon={alertCircle} />
-                  <h2>
-                    <T>Warning</T>
-                  </h2>
-                </div>
-
-                <p>
-                  <T>
-                    Blue-Green algae (also known as cyanobacteria) can be
-                    harmful to the health of people and animals. Do NOT touch or
-                    ingest anything you suspect to be a bloom and do not allow
-                    pets or children to come into contact with, or swallow, the
-                    water.
-                  </T>
-                </p>
-
-                <p>
-                  <T>
-                    By clicking “I Understand” you agree that you have read and
-                    understood this warning
-                  </T>
-                </p>
-
-                <IonButton fill="clear" onClick={exit}>
-                  <T>I Understand</T>
-                  <IonIcon slot="end" icon={arrowForward} />
-                </IonButton>
+            <div className="message-container">
+              <div className="message-header">
+                <IonIcon icon={alertCircleOutline} />
+                <h2>
+                  <T>Warning</T>
+                </h2>
               </div>
+
+              <p>
+                <T>
+                  Blue-Green algae (also known as cyanobacteria) can be harmful
+                  to the health of people and animals. Do NOT touch or ingest
+                  anything you suspect to be a bloom and do not allow pets or
+                  children to come into contact with, or swallow, the water.
+                </T>
+              </p>
+
+              <p>
+                <T>
+                  By clicking “I Understand” you agree that you have read and
+                  understood this warning
+                </T>
+              </p>
+
+              <IonButton onClick={exit} color="secondary">
+                <T>I Understand</T>
+              </IonButton>
             </div>
           </IonSlide>
         </IonSlides>
       </Main>
+      {!isEnd && (
+        <IonFooter className="ion-no-border">
+          <IonToolbar>
+            <IonButtons slot="end">
+              <IonButton onClick={slideNextOrClose}>
+                <IonIcon icon={arrowForward} />
+              </IonButton>
+            </IonButtons>
+          </IonToolbar>
+        </IonFooter>
+      )}
     </Page>
   );
 };
